8679f7469723c48c333671db15cfbc934abf8eac
SamratCoder710 <85514305+SamratCoder710@users.noreply.github.com>
2025-01-12 01:26:16 +0530

    Update billCalculator.py

--- billCalculator.py
+++ billCalculator.py
@@ -2,7 +2,7 @@ import math
 
 print("Welcome to Bill Calculator!")
 total_bill = float(input("What was the total bill?\n$"))
-tip = float(input("How much tip would you like to give ? 10, 12, or 15?\n"))
+tip = int(input("How much tip would you like to give ? 10, 12, or 15?\n"))
 people = int(input("How many people to split the bill?\n"))
 total_bill += (tip * total_bill)/100
 per_person_bill = round(total_bill/people,2)
